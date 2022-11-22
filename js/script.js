1a9abba89cf705fded85c400d69cb4f78bab40a3
gab-che <gabriellachessa1@gmail.com>
2022-11-22 16:36:22 +0100

    adds @click on buttons

--- js/script.js
+++ js/script.js
@@ -11,6 +11,20 @@ createApp({
     },
 
     methods:{
+        prevImg(){
+            if(this.currentIndex === 0){
+                this.currentIndex = this.slides.length -1;
+            } else{
+                this.currentIndex--;
+            }
+        },
 
+        nextImg(){
+            if(this.currentIndex === this.slides.length - 1){
+                this.currentIndex = 0;
+            } else{
+                this.currentIndex++;
+            }
+        }
     }
 }).mount("#app");
